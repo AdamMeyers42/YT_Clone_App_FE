e8a1513c4963bd4cb996a7d3d1758d977e568859
Micheal Moore <michealmoore@Micheals-MacBook-Air.local>
2021-11-09 13:23:04 -0500

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+
+
+class App extends Component {
+    constructor(props){
+      super(props);
+      this.state = 
+
+
+    componentDidMount = () => {
+        this.getVideo(this.state.searchTerm);
+        this.getAllComments();
+      }
+
+      render();{
+        return (
+            <h1>Hello World!</h1>
+        )
+      }
+         
+    
+export default App
