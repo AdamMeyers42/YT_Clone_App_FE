b5d99927be13875d770fb31ad9ececca369f25af
AdamMeyers42 <adammeyers3551@gmail.com>
2021-11-11 08:36:00 -0700

    Embedded ytplayer in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,17 @@ import SearchBar from './SearchBar';
 function App() {
     return (
         <div className="App">
-            <SearchBar placeholder="Enter search parameter..." data={''}/>
+            <h1><SearchBar placeholder="Enter search parameter..." data={''}/></h1>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <iframe id="ytplayer" type="text/html" width="640" height="360"
+            src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+            frameborder="0"></iframe>
         </div>
     );
 }
