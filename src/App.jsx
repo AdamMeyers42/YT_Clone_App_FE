16137ad81f39bbfdbc22334774f0db283540adbb
AdamMeyers42 <adammeyers3551@gmail.com>
2021-11-12 08:12:40 -0700

    Pushing for partner

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,12 @@ import KEY from './APIkey'
 import VideoDetail from './Components/VideoDetail/VideoDetails'
 import SearchBar from './Components/SearchBar/SearchBar';
 
-function App() {
-    const [data, setData] = useState([]);
-    const [q, setQ] = useState("");
-
-    useEffect(() => {
-        console.log("useEffect is running")
-        searchVideos();
-    }, [])
-
-    async function searchVideos() {
-        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=top react videos&type=video&key=AIzaSyDmJgZx84PoQlXfu2PjnARjqU2U_XVDQbw&part=snippet`)
-        console.log(response);
-    }
-    const onTermSubmit = async (term) => {
-      setLoading(true);
-      const response = await KEY.get("/search")
 
 function App() {
  
     return (
         <div className="App">
             <h1><SearchBar/></h1>
-            <h1><SearchBar searchTerm={onTermSubmit} /></h1>
             <br />
             <br />
             <br />
@@ -34,11 +17,11 @@ function App() {
             <br />
             <br />
             <br />
-            <iframe id="ytplayer" type="text/html" width="640" height="360"
+            {/* <iframe id="ytplayer" type="text/html" width="640" height="360"
             src="https://www.youtube.com/embed/y-pM7EPQl1s?autoplay=1&origin=http://example.com"
-            frameborder="0"></iframe>
+            frameborder="0"></iframe> */}
         </div>
     )
 }
-}
+
 export default App;
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -6,24 +6,27 @@ import SearchIcon from '@material-ui/icons/Search';
 
 
 function SearchBar() {
-    const [data, setData] = useState([]);
+    const searchQ = []
     const [q, setQ] = useState();
 
     useEffect(() => {
         console.log("useEffect is running")
         searchVideos();
-    }, [q])
+    }, [setQ])
 
-    async function searchVideos() {
-        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${q}&type=video&key=${key}&part=snippet`)
+    const handleSubmit = async (e) =>  setQ(e.target.value)
+
+    const searchVideos = async (e) => {
+        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q= ${q} &type=video&key=${key}&part=snippet`)
         console.log(response);
+        return searchQ = response.data.videoId
     }
 
     return (
         <div className="search">
             <div className="searchInputs">
-            <form>                                            
-            <input type="text" value={q} onChange={(e) => setQ(e.target.value)} class="st-search-input search-field" />
+            <form onSubmit={handleSubmit} >                                            
+            <input type="text" onChange={(e) => setQ(e.target.value)} />
             <button type="submit">Submit</button>
             </form>
                 <div className="searchIcon">
