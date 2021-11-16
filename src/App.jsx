cd91f085c7344ef8cdec8dbf039cd6ce05a81932
AdamMeyers42 <adammeyers3551@gmail.com>
2021-11-16 07:11:50 -0700

    Able to get comments to push to db (comment and vidoe_id)

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import { key } from './APIkey';
 import VideoDetail from './Components/VideoDetail/VideoDetails';
 import VideoList from './Components/VideoList/VideoList';
 import SearchBar from './Components/SearchBar/SearchBar';
-import youtube from './API/youtube'
-import Comments from './Components/Comments/Comments';
-import { Grid } from '@material-ui/core'
+import youtube from './API/youtube';
+import axios from 'axios';
+
 
 function App() {
     const [videos, setVideos] = useState([])
     const [selectedVideo, setSelectedVideo] = useState()
-    const initialComments = [{ id: 1, text: "This is the first comment" }, { id: 2, text: "This is the second comment" }]
+    // const [comments, setComments] = useState()
 
     const handleSubmit = async (searchTerm) => {
         const response = await youtube.get('search', {
@@ -32,14 +32,47 @@ function App() {
 
     useEffect(() => {
         setSelectedVideo(videos[0]);
+        // setComments();
     }, [videos]);
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        const response = axios.post(
+            'http://localhost:8000/comments/',
+            {
+                video_id: e.target[0].value,
+                comment: e.target[1].value
+            }
+        )
+           const comments=getComments(e.target[0].value)
+           console.log('here are my new comments')
+           console.log(comments)
+
+      }
+    
+    const getComments = (videoId) => {
+        const response = axios.get('http://localhost:8000/comments/'+videoId)
+        console.log('here are my comments')
+        console.log(response)
+        return response.data
+        // return setComments(response.data)
+    }
 
     return (
         <div>
             <SearchBar handleSubmit={handleSubmit} /> <br /> <br /><br /><br /><br /><br />
             <VideoDetail video={selectedVideo}/> <br /> <br />
-            <Comments comments={initialComments} />
             <br /> <br /><br /><br /><br /><br />
+            <form onSubmit={onSubmit}>
+                <input type="hidden" id="video_id" name="video_id" value={selectedVideo != null ? selectedVideo.id.videoId : ''} />
+                <input type="text" name="comment" placeholder="Add Comment Here" />
+                <button type="submit" value="new" id='submit'>Add Comment</button>
+            </form>
+            <ul>        
+                <li>Comment One</li>
+                <li>Comment Two</li>
+                <li>Comment Three</li>
+            </ul>
             <VideoList videos={videos} setSelectedVideo={setSelectedVideo} onVideoSelect={onVideoSelect} />
         </div>
     )
